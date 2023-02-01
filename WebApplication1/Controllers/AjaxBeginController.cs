60737e9a5df68ab2828cfe72ec3a7c188be0151b
Aysu Nur Terzi <aysunurterzi@gmail.com>
2023-02-01 15:51:29 +0300

    Add project files.

---
+++ WebApplication1/Controllers/AjaxBeginController.cs
@@ -0,0 +1,53 @@
+﻿using WebApplication1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+
+namespace WebApplication1.Controllers
+{
+    public class AjaxBeginController : Controller
+    {
+        // GET: AjaxBegin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+
+            UserCreateViewModel model = new UserCreateViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        public JsonResult Create(UserCreateViewModel model)
+        {
+            Thread.Sleep(3000);
+
+            JsonResultModel jModel = new JsonResultModel();
+            try
+            {
+                WebApplication1.Models.ExampleDBEntities db = new Models.ExampleDBEntities();
+                WebApplication1.Models.Users userModel = new Models.Users();
+                userModel.UserName = model.UserName;
+                userModel.UserSurname = model.UserSurname;
+                userModel.City = model.City;
+                db.Users.Add(userModel);
+                db.SaveChanges();
+                jModel.IsSuccess = true;
+                jModel.UserMessage = "Kayıt Başarıyla Gerçekleşti.";
+            }
+            catch (Exception ex)
+            {
+                jModel.IsSuccess = false;
+                jModel.UserMessage = "Kayıt Gerçekleşemedi. Hata: " + ex.Message;
+            }
+            return Json(jModel, JsonRequestBehavior.AllowGet);
+            
+        }
+
+    }
+}
---
+++ WebApplication1/ViewModel/UserCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModel
+{
+    public class UserCreateViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Bir İsim Giriniz")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Bir Soyisim Giriniz")]
+        public string UserSurname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şehir Giriniz")]
+        public string City { get; set; }
+    }
+}
